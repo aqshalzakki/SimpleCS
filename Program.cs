148eede40fc50cd343c2da37bf26d2401e8b827f
Aqshal Zakki <46075252+aqshalzakki@users.noreply.github.com>
2023-05-15 16:35:41 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Simple
+{
+    class Simple
+    {
+        public string name = "John Doe";
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine(GetDayName(2));
+            return 0;
+        }
+
+        static string GetDayName(int num)
+        {
+            string[] dayNames = {
+                "Senin",
+                "Selasa",
+                "Rabu",
+                "Kamis",
+                "Jumat",
+                "Sabtu",
+                "Minggu"
+            };
+            return dayNames[num];
+        }
+    }
+
+}
